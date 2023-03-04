99a9bff97be3cdd6a8ad925b1fae04b30a745e02
mayur-chace <mayur.kamthe@lockrfinance.com>
2023-03-04 19:38:08 +0530

    css fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import HomeComp from '../components/HomeComp/HomeComp'
 import Layout from '../components/Layout/Layout'
-// import Banner from '../components/Banner/Banner'
-// import Navbar from '../components/Navbar/Navbar'
 
 function Home() {
   return (
